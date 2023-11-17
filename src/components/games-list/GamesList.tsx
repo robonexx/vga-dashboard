0618ea61d55f29e11a32d2e247a9773d9c841cfb
robonexx <robertwagar@gmail.com>
2023-11-17 13:46:58 +0100

    simple game animation

--- src/components/games-list/GamesList.tsx
+++ src/components/games-list/GamesList.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { motion, Variants } from 'framer-motion';
 import GameItem from './GameItem';
 import { GameItemProps } from '../../types/Types';
 
@@ -8,7 +9,7 @@ interface GamesListProps {
   setModalOpen: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-const GamesContainer = styled.ul`
+const GamesContainer = styled(motion.ul)`
   position: relative;
   margin-top: 2.625rem;
   display: flex;
@@ -17,13 +18,30 @@ const GamesContainer = styled.ul`
   list-style: none;
 `;
 
+const itemVariants: Variants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+};
+
 const GamesList: React.FC<GamesListProps> = ({ games, setModalOpen }) => {
   return (
     <>
-      <GamesContainer>
+      <GamesContainer
+        initial='hidden'
+        animate='visible'
+        variants={{
+          visible: {
+            transition: {
+              staggerChildren: 0.1, // Adjust the stagger delay as needed
+            },
+          },
+        }}
+      >
         {games.length > 0 &&
           games.map((game) => (
-            <GameItem game={game} key={game.id} setModalOpen={setModalOpen} />
+            <motion.li key={game.id} variants={itemVariants}>
+              <GameItem game={game} setModalOpen={setModalOpen} />
+            </motion.li>
           ))}
       </GamesContainer>
     </>
